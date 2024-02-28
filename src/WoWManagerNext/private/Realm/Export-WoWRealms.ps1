93d3039f88676624e47e46bbb460104919458ccf
Marius Storhaug <marstor@hotmail.com>
2024-02-28 11:43:20 +0100

    Remove WoWManagerNext

--- src/WoWManagerNext/private/Realm/Export-WoWRealms.ps1
+++
@@ -1,3 +0,0 @@
-Function Export-WoWRealms {
-    Get-WoWRealm | ConvertTo-Json -EnumsAsStrings | Out-File -FilePath "$Script:WoW_Folder_Cache\Realms.json" -Force
-}
--- src/WoWManagerNext/public/Addon/Show-WoWAddon.ps1
+++
@@ -1,6 +0,0 @@
-Function Show-WoWAddon {
-    [CmdletBinding()]
-    [OutputType([void])]
-    param()
-    $Script:WoW_Addons | Out-GridView -Title "WoWManager - Installed WoW Addons"
-}
