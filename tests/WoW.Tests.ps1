c62b5b251e9a0638ae1be5c62cf349cd2b811476
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:10:44 +0100

    Align

---
+++ tests/WoW.Tests.ps1
@@ -0,0 +1,23 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'Test code only'
+)]
+[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Write-Verbose "Path to the module: [$Path]" -Verbose
+
+Describe 'WoW' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'WoW' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'WoW' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be importable' {
+            { Import-Module -Name 'WoW' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
+        }
+    }
+}
