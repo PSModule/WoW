0cdcd0d8395f5018d32080a85b983611cf26c542
Marius Storhaug <marstor@hotmail.com>
2024-04-14 01:50:19 +0200

    Remove module import test

--- tests/WoW.Tests.ps1
+++ tests/WoW.Tests.ps1
@@ -11,13 +11,4 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'WoW' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'WoW' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'WoW' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'WoW' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
 }
