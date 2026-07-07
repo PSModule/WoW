2e9172d5cb183481a5c1b467b81a5e65ea9d4b33
Marius Storhaug <marstor@hotmail.com>
2026-07-07 17:44:58 +0200

    Require Pester 6.x in test files

--- tests/WoW.Tests.ps1
+++ tests/WoW.Tests.ps1
@@ -1,4 +1,6 @@
-﻿[CmdletBinding()]
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.*'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+
+[CmdletBinding()]
 param()
 
 Describe 'WoW' {
